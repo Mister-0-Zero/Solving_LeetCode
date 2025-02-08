64eee801ae1287ad9adfd5a5cafa871128c7c073
Mister-0-Zero <alexyulyadkov@gmail.com>
2025-02-08 16:45:46 +0300

    60

--- LeetCode/51-75/51-60/59.py
+++ LeetCode/51-75/51-60/59.py
@@ -1,6 +1,6 @@
 class Solution:
     def generateMatrix(self, n: int) -> list[list[int]]:
-        matrix = [[0 for _ in range(n)] for _ in range(n)]
+        matrix = [[0 for _ in range(n)] for _ in range(n)] #формируем матрицу нулей
         column, row = -1, 0
         dcol, drow = 1, 0
 
@@ -11,7 +11,8 @@ class Solution:
             matrix[row][column] = score
 
             if column + dcol == n or row + drow == n or matrix[row + drow][column + dcol] != 0:
-                dcol, drow = -drow, dcol
+                dcol, drow = -drow, dcol #суть просто, идем до конца строки или столбца или до заполнилненого поля
+                #а потом меняем направление движения, чтобы идти дальше по часовой стрелке
 
         return matrix
 
