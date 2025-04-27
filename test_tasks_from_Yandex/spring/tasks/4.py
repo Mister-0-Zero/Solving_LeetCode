569c1cdc46d87850abcb4ff7112c4b6151f7bfe5
Mister-0-Zero <alexyulyadkov@gmail.com>
2025-04-27 17:38:41 +0300

    mini-change in 4 number

--- test_tasks_from_Yandex/spring/tasks/4.py
+++ test_tasks_from_Yandex/spring/tasks/4.py
@@ -1,52 +1,71 @@
 from datetime import datetime, timedelta
 
-def parsing_data(kol_str, ind_sort):
+def parse_data(num_entries: int, sort_index: int):
+    """Читает данные и сортирует по нужному индексу."""
     data = []
-    for i in range(kol_str):
+    for _ in range(num_entries):
         number, time_start, time_end, cost = input().split()
         time_start = datetime.strptime(time_start, "%H:%M:%S")
         time_end = datetime.strptime(time_end, "%H:%M:%S")
         cost = int(cost)
         data.append([number, time_start, time_end, cost])
-    return sorted(data, key=lambda x: x[ind_sort])
+    return sorted(data, key=lambda x: x[sort_index])
 
-N = int(input())
-data_el = parsing_data(N, 2)
-M = int(input())
-data_train = parsing_data(M, 1)
+def prepare_min_cost(data_train):
+    """Предрасчитывает минимальные стоимости поездов справа налево."""
+    min_cost_from_here = [None] * len(data_train)
+    min_cost_from_here[-1] = data_train[-1]
+    for i in range(len(data_train) - 2, -1, -1):
+        if data_train[i][3] <= min_cost_from_here[i + 1][3]:
+            min_cost_from_here[i] = data_train[i]
+        else:
+            min_cost_from_here[i] = min_cost_from_here[i + 1]
+    return min_cost_from_here
 
-# print(data_el)
-# print(data_train)
+def find_next_train(data_train, min_cost_from_here, end_time, start_index):
+    """Находит подходящий поезд с минимальной стоимостью."""
+    min_start_time = end_time + timedelta(minutes=15)
+    while start_index < len(data_train) and data_train[start_index][1] < min_start_time:
+        start_index += 1
 
-def cost_train(end):
-    global data_train
-    ind = 0
-    min_start_train_time = end + timedelta(minutes=15)
-    while ind < len(data_train) and data_train[ind][1] < min_start_train_time:
-        ind += 1
+    if start_index == len(data_train):
+        return None, float("inf"), start_index
 
-    if ind == len(data_train):
-        return None, float('inf')
+    num, start, end, cost = min_cost_from_here[start_index]
+    return num, cost, start_index
 
-    data_train = data_train[ind:]
-    num, start, end, cost = min(data_train, key=lambda x: x[3])
-    return cost, num
+def main():
+    N = int(input())
+    data_el = parse_data(N, 2)
 
-ind = 0
-min_cost = float('inf')
-i = 0
-cost_tr, num_tr = cost_train(data_el[0][2])
-b_num_el, b_num_tr = data_el[0][0], num_tr
-for num, start, end, cost in data_el:
-    if cost < data_el[ind][3]:
-        cost_tr, num_tr = cost_train(end)
-        if cost + cost_tr < min_cost:
-            ind = i
-            b_num_el = num
-            b_num_tr = num_tr
-            min_cost = cost + cost_tr
-    i += 1
+    M = int(input())
+    data_train = parse_data(M, 1)
 
-print(b_num_el)
-print(b_num_tr)
+    min_cost_from_here = prepare_min_cost(data_train)
 
+    best_el = None
+    best_train = None
+    current_element_index = 0
+    train_index = 0
+
+    # Предрассчитаем первый поезд
+    first_num_tr, first_cost_tr, train_index = find_next_train(data_train, min_cost_from_here, data_el[0][2], train_index)
+    best_el = data_el[0][0]
+    best_train = first_num_tr
+
+    min_total_cost = data_el[0][3] + first_cost_tr
+
+    for i, (num, start, end, cost) in enumerate(data_el):
+        if cost < data_el[current_element_index][3]:
+            num_tr, cost_tr, train_index = find_next_train(data_train, min_cost_from_here, end, train_index)
+            if cost + cost_tr < min_total_cost:
+                current_element_index = i
+                best_el = num
+                best_train = num_tr
+                min_total_cost = cost + cost_tr
+
+    print(best_el)
+    print(best_train)
+
+if __name__ == "__main__":
+    main()
